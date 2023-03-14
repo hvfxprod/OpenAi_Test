75291165ad25806c71bf6a13e721388c962b4366
Myounghoon Lee <122421131+hvfxprod@users.noreply.github.com>
2023-03-14 10:51:35 +0900

    Create application.py
    Basic(except for GUI)

---
+++ application.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import openai
+import re, traceback
+
+Completion.traceback(
+  [
+    { 
+    "user": "user"
+    "contents": "helpful advice and solution for someone who learn about VFX(visual effects), metaverse, AI(artificial Inteligence)"
+    }
+  ]
+)
+
+main(loop)
